83cb1a46c8112049f2a0c40b4669d47ce5b66b6e
Vincent Masselis <vincent.masselis@gmail.com>
2022-05-30 21:52:25 +0200

    Update common.sh

--- common.sh
+++ common.sh
@@ -4,4 +4,4 @@ brew install --cask logi-options-plus logitech-options mos spotify vlc slack fir
 
 open https://raw.githubusercontent.com/VincentMasselis/auto-setup/main/mx-master-mos-config.png
 open https://raw.githubusercontent.com/VincentMasselis/auto-setup/main/mx-master-system-config.png
-open https://raw.githubusercontent.com/VincentMasselis/auto-setup/main/mx-master-options-config.png
+echo "In order to configure correctly the mouse onto Logitech Options, you should connect into the Option and Option+ app with Google sign-in"
