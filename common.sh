138627e75deeb9660ffb4e21959f2c35fbda7e47
Vincent Masselis <>
2022-05-30 16:37:42 +0200

    Added common file

---
+++ common.sh
@@ -0,0 +1,3 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install logi-options-plus logitech-options mos spotify vlc firefox sublime-text fork invisiblix tableplus
