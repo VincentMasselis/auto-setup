f8079f90e43a8aa631c52252280621659c21b1ba
Vincent Masselis <>
2022-05-30 21:38:55 +0200

    Added options settings

--- common.sh
+++ common.sh
@@ -4,3 +4,4 @@ brew install --cask logi-options-plus logitech-options mos spotify vlc slack fir
 
 open https://raw.githubusercontent.com/VincentMasselis/auto-setup/main/mx-master-mos-config.png
 open https://raw.githubusercontent.com/VincentMasselis/auto-setup/main/mx-master-system-config.png
+open https://raw.githubusercontent.com/VincentMasselis/auto-setup/main/mx-master-options-config.png
