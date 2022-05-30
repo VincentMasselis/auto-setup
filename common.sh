1697e85d5ccc25f75832f12c6e84f1166aa2cfea
Vincent Masselis <>
2022-05-30 16:58:52 +0200

    Fixed bad cask usage

--- common.sh
+++ common.sh
@@ -1,5 +1,5 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew cask install logi-options-plus logitech-options mos spotify vlc firefox sublime-text fork docker invisiblix tableplus
+brew install logi-options-plus logitech-options mos spotify vlc firefox sublime-text fork docker invisiblix tableplus --cask
 
 open mos-config.png
