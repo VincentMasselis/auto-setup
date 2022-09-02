9a3131f27089ae6059df54fc66a3d4da04fb6409
Vincent Masselis <vincent.masselis@gmail.com>
2022-09-02 12:36:48 +0200

    Update common.sh

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install --cask logi-options-plus logitech-options mos spotify vlc slack firefox invisiblix
+brew install --cask homebrew/cask-drivers/logi-options-plus homebrew/cask-drivers/logitech-options mos spotify vlc slack firefox invisiblix bitwarden
 
 open https://raw.githubusercontent.com/VincentMasselis/auto-setup/main/mx-master-mos-config.png
 open https://raw.githubusercontent.com/VincentMasselis/auto-setup/main/mx-master-system-config.png
