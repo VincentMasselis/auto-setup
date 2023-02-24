cde64b1fa169be351a65578324eb6b9ef6652228
Vincent Masselis <vincent.masselis@gmail.com>
2023-02-24 17:26:56 +0100

    Update android.sh

--- android.sh
+++ android.sh
@@ -1,4 +1,4 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/VincentMasselis/auto-setup/main/dev.sh)"
 
-brew install --cask android-file-transfer android-platform-tools android-studio
+brew install --cask android-file-transfer android-commandlinetools android-studio
 brew install git-crypt scrcpy
