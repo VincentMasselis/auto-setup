1c7ded20b204f0801324fba089427e81a4ceb674
Vincent Masselis <vincent.masselis.partner@decathlon.com>
2023-06-02 09:33:02 +0200

    Updated to the current config

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install --cask homebrew/cask-drivers/logi-options-plus homebrew/cask-drivers/logitech-options mos spotify vlc slack firefox invisiblix bitwarden
+brew install --cask homebrew/cask-drivers/logi-options-plus spotify vlc slack firefox bitwarden
 
 open https://raw.githubusercontent.com/VincentMasselis/auto-setup/main/mx-master-mos-config.png
 open https://raw.githubusercontent.com/VincentMasselis/auto-setup/main/mx-master-system-config.png
