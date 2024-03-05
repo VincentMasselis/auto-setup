bad3a015a62caca3310ac3c8b6752659f9ef29d6
Vincent Masselis <vincent.masselis@gmail.com>
2024-03-05 09:40:10 +0100

    Update common.sh

--- common.sh
+++ common.sh
@@ -1,7 +1,3 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install --cask homebrew/cask-drivers/logi-options-plus spotify vlc slack firefox bitwarden
-
-open https://raw.githubusercontent.com/VincentMasselis/auto-setup/main/mx-master-mos-config.png
-open https://raw.githubusercontent.com/VincentMasselis/auto-setup/main/mx-master-system-config.png
-echo "In order to configure correctly the mouse onto Logitech Options, you should connect into the Option and Option+ app with Google sign-in"
+brew install --cask spotify vlc slack firefox bitwarden autodesk-fusion	
