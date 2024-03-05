78ccc5a0c88ecda3d8fab22c4e8321a867e7d4c2
Vincent Masselis <vincent.masselis@gmail.com>
2024-03-05 09:39:37 +0100

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,3 +1,3 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/VincentMasselis/auto-setup/main/common.sh)"
 
-brew install --cask sublime-text fork docker tableplus
+brew install --cask sublime-text fork docker tableplus nordvpn
